3b207c7b53bde17825d433ae22c6ce1448a7897b
liujie <liujie@huxiaosu.com>
2019-05-10 18:32:19 +0800

    config-server -test...

--- spring-cloud/spring-cloud-service-one/src/main/java/com/huxiaosu/demo/cloud/service/one/controller/DemoController.java
+++ spring-cloud/spring-cloud-service-one/src/main/java/com/huxiaosu/demo/cloud/service/one/controller/DemoController.java
@@ -8,9 +8,12 @@
  */
 package com.huxiaosu.demo.cloud.service.one.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Description
@@ -22,9 +25,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
+    @Value("${test}")
+    String serviceOne;
+
     @GetMapping("/test")
     @ResponseBody
     public String test() {
-        return "spring-cloud-service-one.DemoController";
+
+        return serviceOne+"-DemoController";
     }
 }
