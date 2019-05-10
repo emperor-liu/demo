fd33cb89999aa4fc6d2d334982c18e5cae28b641
liujie <liujie@huxiaosu.com>
2019-05-11 01:11:42 +0800

    监听文件修改状态

--- spring-cloud/spring-cloud-service-one/src/main/java/com/huxiaosu/demo/cloud/service/one/SpringCloudServiceOneApplication.java
+++ spring-cloud/spring-cloud-service-one/src/main/java/com/huxiaosu/demo/cloud/service/one/SpringCloudServiceOneApplication.java
@@ -2,8 +2,10 @@ package com.huxiaosu.demo.cloud.service.one;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
 public class SpringCloudServiceOneApplication {
 
 	public static void main(String[] args) {
