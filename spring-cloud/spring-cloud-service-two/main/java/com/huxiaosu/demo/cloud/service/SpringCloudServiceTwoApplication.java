3f4cd358d09297805f42cc842c35d50e66662107
liujie <liujie@huxiaosu.com>
2019-05-10 11:26:40 +0800

    spring-cloud

---
+++ spring-cloud/spring-cloud-service-two/main/java/com/huxiaosu/demo/cloud/service/SpringCloudServiceTwoApplication.java
@@ -0,0 +1,13 @@
+package com.huxiaosu.demo.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudServiceTwoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudServiceTwo.class, args);
+	}
+
+}
